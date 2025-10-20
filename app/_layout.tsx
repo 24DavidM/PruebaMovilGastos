21fea84bb243b27dd51e2fd33541174841562bba
24DavidM <stalin.muela02@epn.edu.ec>
2025-10-20 15:48:35 -0500

    first commit

---
+++ app/_layout.tsx
@@ -0,0 +1,27 @@
+import { Stack, useRouter } from 'expo-router';
+import React from 'react';
+import { View } from 'react-native';
+import BottomTabs from '@/components/BottomTabs';
+import "../global.css";
+import { GastosProvider } from '@/context/GastosContext';
+
+type ScreenKey = 'inicio' | 'balance' | 'recibo' | 'reportes';
+
+export default function Layout() {
+    const router = useRouter();
+    const [activeScreen, setActiveScreen] = React.useState<ScreenKey>('inicio');
+
+    const handleTabPress = (key: ScreenKey) => {
+        setActiveScreen(key);
+        router.push(`/${key}`);
+    };
+
+    return (
+        <GastosProvider>
+            <View style={{ flex: 1 }}>
+                <Stack screenOptions={{ headerShown: false }} />
+                <BottomTabs onTabPress={handleTabPress} />
+            </View>
+        </GastosProvider>
+    );
+}
